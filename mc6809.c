59b263ce44f7ab652c89bbbaa04c85249f08ac46
Abathur8bit <abathur8bit@8bitcoder.com>
2018-03-10 15:46:11 -0500

    FFFF was already being loaded, so we just need to check the source is valid.

--- mc6809.c
+++ mc6809.c
@@ -891,16 +891,11 @@ case TFR_M: //1F
 			}
 			else if (Source <= 7)
 			{
-				//If an invalid register encoding is used for the source, 
-				//a constant value of FFFF is transferred to the destination.
+				//make sure the source is valud
 				if (xfreg16[Source])
 				{
 					*xfreg16[Dest] = *xfreg16[Source];
 				}	
-				else
-				{
-					*xfreg16[Dest] = 0xFFFF;
-				}
 			}
 		break;
 
