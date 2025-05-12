5d2375542767e4f589db010518c6cdf97231e623
Craig Allsop <csallsopweb+git@gmail.com>
2025-05-12 12:48:42 +1000

    Refactor DirectX specific code into its own class, like OpenGL

--- BuildConfig.h
+++ BuildConfig.h
@@ -27,12 +27,19 @@
 
 
 //
-// Enable the use of OpenGL display driver, otherwise use DirectX.
+// Enable the use of OpenGL display driver
 //
 #ifndef _LEGACY_VCC
 #ifndef USE_OPENGL
 #define USE_OPENGL true
 #endif
+#endif
+
+//
+// Enable the use of DirectX display driver
+//
+#ifndef USE_DIRECTX
+#define USE_DIRECTX true
 #endif
 
 //
@@ -84,13 +91,6 @@
 // 
 // Edit options above rather than these:
 //
-#if !USE_OPENGL
-#define USE_DIRECTX true
-#endif
-
-#if USE_OPENGL && USE_DIRECTX
-#error Enable either USE_OPENGL or USE_DIRECTX not both.
-#endif
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Options that are always off for release
--- OpenGL.cpp
+++ OpenGL.cpp
@@ -87,15 +87,17 @@ namespace VCC
 		GLuint texId; // OpenGL texture on gpu
 		wglprocSwapIntervalEXT wglSwapIntervalEXT;
 
+		ISystemState* state;
+
 		#if USE_DEBUG_LINES
 		struct Line { float x1; float y1; float x2; float y2; Pixel color; };
 		std::vector<Line> debugLines;
 		#endif // USE_DEBUG_LINES
 
-		OpenGL::Detail()
+		OpenGL::Detail(ISystemState *state)
 			: isInitialized(false), hWnd(NULL), hDC(NULL), hRC(NULL)
 			, width(0), height(0), statusHeight(0), aspect(true), ntsc(false)
-			, pixels(nullptr), texId(0), wglSwapIntervalEXT(nullptr)
+			, pixels(nullptr), texId(0), wglSwapIntervalEXT(nullptr), state(state)
 		{
 		}
 
@@ -446,6 +448,7 @@ namespace VCC
 			{
 				case OPT_FLAG_ASPECT: aspect = enabled; break;
 				case OPT_FLAG_NTSC: ntsc = enabled; break;
+				case OPT_FLAG_RESIZEABLE: break;
 				default: return Result(ERR_BADOPTION);
 			}
 			return Result(OK);
@@ -643,6 +646,13 @@ namespace VCC
 			return Result(OK);
 		}
 
+		int LockSurface()
+		{
+			if (!isInitialized) return Result(ERR_NOTINITIALIZED);
+			state->SetSurface(pixels, 3, DefaultWidth);
+			return Result(OK);
+		}
+
 	private:
 
 		// returns the box where display should be rendered
@@ -698,9 +708,9 @@ namespace VCC
 		return Result(ERR_NOTINITIALIZED);
 	}
 
-	int OpenGL::Setup(void* hwnd, int width, int height, int statusHeight)
+	int OpenGL::Setup(void* hwnd, int width, int height, int statusHeight, bool fullscreen)
 	{
-		detail = new Detail();
+		detail = new Detail(state);
 		return detail->Setup(hwnd, width, height, statusHeight);
 	}
 
@@ -762,6 +772,13 @@ namespace VCC
 	{
 		if (detail)
 			return detail->LoadFont(outFont, bitmapRes, glyphs, start, end);
+		return Result(ERR_NOTINITIALIZED);
+	}
+
+	int OpenGL::LockSurface()
+	{
+		if (detail)
+			return detail->LockSurface();
 		return Result(ERR_NOTINITIALIZED);
 	}
 
