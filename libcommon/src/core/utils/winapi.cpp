0a7cfc49dc0a3e64e48d0d841558d07ff4f92fef
ejaquay <ejjaquay@gmail.com>
2025-10-23 18:28:26 -0400

    Set project to C++17
    vcc-base.props set to C++17
    
    core/utils/winapi.cpp modified to demonstrate C++17 namespace nesting
    and to fix STL4017.  Still don't like the winapi.cpp filename but that
    perhaps is for another commit.

--- libcommon/src/core/utils/winapi.cpp
+++ libcommon/src/core/utils/winapi.cpp
@@ -15,23 +15,36 @@
 //	You should have received a copy of the GNU General Public License along with
 //	VCC (Virtual Color Computer). If not, see <http://www.gnu.org/licenses/>.
 ////////////////////////////////////////////////////////////////////////////////
+
 #include <vcc/core/utils/winapi.h>
-#include <codecvt>
+#include <string>
+#include <windows.h>
 
-
-namespace vcc { namespace core { namespace utils
+namespace vcc::core::utils
 {
-
 	LIBCOMMON_EXPORT std::string load_string(HINSTANCE instance, UINT id)
 	{
-		LPWSTR buffer_ptr = nullptr;
-		const auto buffer_length(LoadStringW(instance, id, reinterpret_cast<LPWSTR>(&buffer_ptr), 0));
-		if (buffer_length < 1 || buffer_ptr == nullptr)
-		{
-			return { };
-		}
+		// Get len of string to load
+		const int length = LoadStringW(instance, id, nullptr, 0);
+		if (length == 0)
+			return {};
 
-		return std::wstring_convert<std::codecvt_utf8<wchar_t>>().to_bytes(buffer_ptr, buffer_ptr + buffer_length);
+		// Copy load string to wide_str
+		std::wstring wide_str(length, L'\0');
+		const int copied = LoadStringW(instance, id, wide_str.data(), length + 1);
+		if (copied == 0)
+			return {};
+
+		// Get len of string when converted
+		const int utf8_len = WideCharToMultiByte(CP_UTF8, 0, wide_str.data(), copied, nullptr, 0, nullptr, nullptr);
+			if (utf8_len == 0)
+			return {};
+
+		// Convert string from wide_str to utf8_str
+		std::string utf8_str(utf8_len, '\0');
+		WideCharToMultiByte(CP_UTF8, 0, wide_str.data(), copied, utf8_str.data(), utf8_len, nullptr, nullptr);
+
+		return utf8_str;
 	}
+}
 
-} } }
