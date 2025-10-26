63715a296d1724f7540d211eac9f8d87a475709f
Chet Simpson <3150309+ChetSimpson@users.noreply.github.com>
2025-10-25 17:41:51 -0700

    add member variables to cloud9 rtc

--- libcommon/include/vcc/devices/rtc/cloud9.h
+++ libcommon/include/vcc/devices/rtc/cloud9.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vcc/core/detail/exports.h>
+#include <Windows.h>
 
 
 namespace vcc::devices::rtc
@@ -11,7 +12,26 @@ namespace vcc::devices::rtc
 
 		void set_read_only(bool value);
 
-		unsigned char read_port(unsigned short port_id) const;
+		unsigned char read_port(unsigned short port_id);
+
+
+	private:
+
+		void set_time();
+
+
+	private:
+
+		SYSTEMTIME now;
+		unsigned __int64 InBuffer = 0;
+		unsigned __int64 OutBuffer = 0;
+		unsigned char BitCounter = 0;
+		unsigned char TempHour = 0;
+		unsigned char AmPmBit = 0;
+		unsigned __int64 CurrentBit = 0;
+		unsigned char FormatBit = 0; //1 = 12Hour Mode
+		unsigned char CookieRecived = 0;
+		unsigned char WriteEnabled = 0;
 	};
 
 }
