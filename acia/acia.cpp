VCCE/VCC

commit eda4552b9fe3250debcb06edfa4b4b07805feaf1

Refactor C API cartridge interface
This change-set removes several exported functions from existing cartridges and replaces them with a single initialization call. It also changes the various callback functions to take an additional argument representing a host key (allowing the host to route the calls). It also renames existing functions exported by the cartridge so they use sensible names and have symmetry.

Update all cartridges to export the PakInitialize, PakGetName, and `PakGetCatalogId` for the new C interface
Changes assert interrupt callback to take an additional argument of type `void*` as a host key.
Changes assert cartridge line callback to take an additional argument of type `void*` as a host key.
Changes write to system memory callback to take an additional argument of type `void*` as a host key.
Changes read from system memory callback to take an additional argument of type `void*` as a host key.
Changes append menu item callback to take an additional argument of type `void*` as a host key.
Remove `MemPointers` implementations from all cartridges (replaced by `PakInitialize`).
Remove `AssertInterupt` implementations from all cartridges (replaced by `PakInitialize`).
Remove `SetIniPath` implementations from all cartridges (replaced by `PakInitialize`).
Remove `SetCart` implementations from all cartridges (replaced by `PakInitialize`).
Remove `ModuleName` implementations from all cartridges (replaced by `PakInitialize`, `PakGetName`, and `PakGetCatalogId`).
Rename `ModuleReset` to `PakReset` in all cartridges that export it.
Rename `HeartBeat` to `PakProcessHorizontalSync` in all cartridges that export it.
Rename `ModuleStatus` to `PakGetStatus` in all cartridges that export it.
Rename `PackPortWrite` to `PakWritePort` in all cartridges that export it.
Rename `PackPortRead` to `PwkReadPort` in all cartridges that export it.
Rename `PakMemRead8` to `PakReadMemoryByte` in all cartridges that export it.

acia/acia.cpp
@@ -23,7 +23,6 @@
 
 #include "acia.h"
 #include "../CartridgeMenu.h"
-#include <vcc/core/legacy_cartridge_definitions.h>
 #include <vcc/core/limits.h>
 #include <vcc/common/DialogOps.h>
 #include <vcc/common/logger.h>
@@ -32,20 +31,27 @@
 // Local Functions
 //------------------------------------------------------------------------
 
-void BuildCartMenu();
+void BuildCartridgeMenu();
 
 LRESULT CALLBACK Config(HWND, UINT, WPARAM, LPARAM);
 void LoadConfig();
 void SaveConfig();
 
 //------------------------------------------------------------------------
+// Pak host
+//------------------------------------------------------------------------
+static void* gHostKeyPtr = nullptr;
+void* const& gHostKey(gHostKeyPtr);
+static PakAppendCartridgeMenuHostCallback CartMenuCallback = nullptr;
+PakAssertInteruptHostCallback AssertInt = nullptr;
+
+//------------------------------------------------------------------------
 // Globals
 //------------------------------------------------------------------------
-static HINSTANCE g_hDLL = nullptr;      // DLL handle
+static HINSTANCE gModuleInstance;      // DLL handle
 static HWND g_hDlg = nullptr;           // Config dialog
 static char IniFile[MAX_PATH];       // Ini file name
 static char IniSect[MAX_LOADSTRING]; // Ini file section
-static AppendCartridgeMenuModuleCallback CartMenuCallback = nullptr;
 
 // Status for Vcc status line
 char AciaStat[32];
@@ -66,7 +72,6 @@ char AciaFileRdPath[MAX_PATH]; // Path for file reads
 char AciaFileWrPath[MAX_PATH]; // Path for file writes
 
 static unsigned char Rom[8192];
-AssertInteruptModuleCallback AssertInt = nullptr;
 unsigned char LoadExtRom(const char *);
 
 //------------------------------------------------------------------------
@@ -76,7 +81,7 @@ BOOL APIENTRY
 DllMain(HINSTANCE hinst, DWORD reason, LPVOID foo)
 {
     if (reason == DLL_PROCESS_ATTACH) {
-        g_hDLL = hinst;
+		gModuleInstance = hinst;
         LoadExtRom("RS232.ROM");
 
     } else if (reason == DLL_PROCESS_DETACH) {
@@ -87,20 +92,41 @@ DllMain(HINSTANCE hinst, DWORD reason, LPVOID foo)
     return TRUE;
 }
 
-//-----------------------------------------------------------------------
-//  Dll export register the DLL and build entry on dynamic menu
-//-----------------------------------------------------------------------
+
 extern "C"
-__declspec(dllexport) void
-ModuleName(char *ModName,char *CatNumber,AppendCartridgeMenuModuleCallback Temp)
 {
-    LoadString(g_hDLL,IDS_MODULE_NAME,ModName,MAX_LOADSTRING);
-    LoadString(g_hDLL,IDS_CATNUMBER,CatNumber,MAX_LOADSTRING);
-    CartMenuCallback = Temp;
-    strcpy(IniSect,ModName);   // Use module name for ini file section
-    if (CartMenuCallback != nullptr) BuildCartMenu();
-    sc6551_init();
-    return ;
+	__declspec(dllexport) const char* PakGetName()
+	{
+		static char string_buffer[MAX_LOADSTRING];
+
+		LoadString(gModuleInstance, IDS_MODULE_NAME, string_buffer, MAX_LOADSTRING);
+
+		return string_buffer;
+	}
+
+	__declspec(dllexport) const char* PakCatalogName()
+	{
+		static char string_buffer[MAX_LOADSTRING];
+
+		LoadString(gModuleInstance, IDS_CATNUMBER, string_buffer, MAX_LOADSTRING);
+
+		return string_buffer;
+	}
+
+	__declspec(dllexport) void PakInitialize(
+		void* const host_key,
+		const char* const configuration_path,
+		const pak_initialization_parameters* const parameters)
+	{
+		gHostKeyPtr = host_key;
+		CartMenuCallback = parameters->add_menu_item;
+		AssertInt = parameters->assert_interrupt;
+		strcpy(IniFile, configuration_path);
+
+		LoadConfig();
+		sc6551_init();
+	}
+
 }
 
 //-----------------------------------------------------------------------
@@ -108,7 +134,7 @@ ModuleName(char *ModName,char *CatNumber,AppendCartridgeMenuModuleCallback Temp)
 //-----------------------------------------------------------------------
 extern "C"
 __declspec(dllexport) void
-PackPortWrite(unsigned char Port,unsigned char Data)
+PakWritePort(unsigned char Port,unsigned char Data)
 {
     sc6551_write(Data,Port);
     return;
@@ -119,7 +145,7 @@ PackPortWrite(unsigned char Port,unsigned char Data)
 //-----------------------------------------------------------------------
 extern "C"
 __declspec(dllexport) unsigned char
-PackPortRead(unsigned char Port)
+PakReadPort(unsigned char Port)
 {
     return sc6551_read(Port);
 }
@@ -128,7 +154,7 @@ PackPortRead(unsigned char Port)
 // Dll export module reset
 //-----------------------------------------------------------------------
 extern "C"
-__declspec(dllexport) void ModuleReset()
+__declspec(dllexport) void PakReset()
 {
     sc6551_close();
     return;
@@ -138,7 +164,7 @@ __declspec(dllexport) void ModuleReset()
 // Dll export pak rom read
 //-----------------------------------------------------------------------
 extern "C"
-__declspec(dllexport) unsigned char PakMemRead8(unsigned short Address)
+__declspec(dllexport) unsigned char PakReadMemoryByte(unsigned short Address)
 {
     return(Rom[Address & 8191]);
 }
@@ -168,19 +194,9 @@ unsigned char LoadExtRom(const char *FilePath)
 // Dll export Heartbeat (HSYNC)
 //-----------------------------------------------------------------------
 extern "C"
-__declspec(dllexport) void HeartBeat()
+__declspec(dllexport) void PakProcessHorizontalSync()
 {
     sc6551_heartbeat();
-    return;
-}
-
-//-----------------------------------------------------------------------
-// Dll export supply transfer point for interrupt
-//-----------------------------------------------------------------------
-extern "C"
-__declspec(dllexport) void AssertInterupt(AssertInteruptModuleCallback Dummy)
-{
-    AssertInt=Dummy;
     return;
 }
 
@@ -188,45 +204,34 @@ __declspec(dllexport) void AssertInterupt(AssertInteruptModuleCallback Dummy)
 // Dll export return module status for VCC status line
 //-----------------------------------------------------------------------
 extern "C"
-__declspec(dllexport) void ModuleStatus(char *status)
+__declspec(dllexport) void PakGetStatus(char* text_buffer, size_t buffer_size)
 {
-    strncpy (status,AciaStat,16);
-    status[16]='\n';
-    return;
+	strncpy(text_buffer, AciaStat, buffer_size);
+	text_buffer[16] = '\n';
 }
 
 //-----------------------------------------------------------------------
 //  Dll export run config dialog
 //-----------------------------------------------------------------------
 extern "C"
-__declspec(dllexport) void ModuleConfig(unsigned char /*MenuID*/)
+__declspec(dllexport) void PakMenuItemClicked(unsigned char /*MenuID*/)
 {
     HWND owner = GetActiveWindow();
-    CreateDialog(g_hDLL,(LPCTSTR)IDD_PROPPAGE,owner,(DLGPROC)Config);
+    CreateDialog(gModuleInstance,(LPCTSTR)IDD_PROPPAGE,owner,(DLGPROC)Config);
     ShowWindow(g_hDlg,1);
     return;
 }
 
-//-----------------------------------------------------------------------
-// Dll export VCC ini file path and load settings
-//-----------------------------------------------------------------------
-extern "C"
-__declspec(dllexport) void SetIniPath (const char *IniFilePath)
-{
-    strcpy(IniFile,IniFilePath);
-    LoadConfig();
-    return;
-}
 
 //-----------------------------------------------------------------------
 //  Add config option to Cartridge menu
 //----------------------------------------------------------------------
-void BuildCartMenu()
+void BuildCartridgeMenu()
 {
-    CartMenuCallback("",MID_BEGIN,MIT_Head);
-    CartMenuCallback("",MID_ENTRY,MIT_Seperator);
-    CartMenuCallback("ACIA Config",ControlId(16),MIT_StandAlone);
-    CartMenuCallback("",MID_FINISH,MIT_Head);
+	CartMenuCallback(gHostKey, "", MID_BEGIN, MIT_Head);
+	CartMenuCallback(gHostKey, "", MID_ENTRY, MIT_Seperator);
+	CartMenuCallback(gHostKey, "ACIA Config", ControlId(16), MIT_StandAlone);
+	CartMenuCallback(gHostKey, "", MID_FINISH, MIT_Head);
 }
 
 //-----------------------------------------------------------------------

FD502/fd502.cpp
@@ -32,7 +32,6 @@ This file is part of VCC (Virtual Color Computer).
 #include "distortc.h"
 #include "fd502.h"
 #include "../CartridgeMenu.h"
-#include <vcc/core/legacy_cartridge_definitions.h>
 #include <vcc/core/limits.h>
 #include <vcc/common/FileOps.h>
 #include <vcc/common/DialogOps.h>
@@ -54,8 +53,10 @@ static unsigned char RGBDiskRom[EXTROMSIZE];
 static char FloppyPath[MAX_PATH];
 static char RomFileName[MAX_PATH]="";
 static char TempRomFileName[MAX_PATH]="";
-AssertInteruptModuleCallback AssertInt = nullptr;
-static AppendCartridgeMenuModuleCallback CartMenuCallback = nullptr;
+static void* gHostKeyPtr = nullptr;
+void* const& gHostKey(gHostKeyPtr);
+PakAssertInteruptHostCallback AssertInt = nullptr;
+static PakAppendCartridgeMenuHostCallback CartMenuCallback = nullptr;
 unsigned char PhysicalDriveA=0,PhysicalDriveB=0,OldPhysicalDriveA=0,OldPhysicalDriveB=0;
 static unsigned char *RomPointer[3]={ExternalRom,DiskRom,RGBDiskRom};
 static unsigned char SelectRom=0;
@@ -73,7 +74,7 @@ long CreateDiskHeader(const char *,unsigned char,unsigned char,unsigned char);
 void Load_Disk(unsigned char);
 
 static HWND g_hConfDlg = nullptr;
-static HINSTANCE g_hinstDLL;
+static HINSTANCE gModuleInstance;
 static unsigned long RealDisks=0;
 long CreateDisk (unsigned char);
 static char TempFileName[MAX_PATH]="";
@@ -97,7 +98,7 @@ BOOL WINAPI DllMain(
 	}
 	else
 	{
-		g_hinstDLL=hinstDLL;
+		gModuleInstance = hinstDLL;
 		RealDisks=InitController();
 	}
 	return 1;
@@ -105,20 +106,44 @@ BOOL WINAPI DllMain(
 
 extern "C"
 {
-	__declspec(dllexport) void ModuleName(char *ModName,char *CatNumber,AppendCartridgeMenuModuleCallback Temp)
+
+	__declspec(dllexport) const char* PakGetName()
 	{
-		LoadString(g_hinstDLL,IDS_MODULE_NAME,ModName, MAX_LOADSTRING);
-		LoadString(g_hinstDLL,IDS_CATNUMBER,CatNumber, MAX_LOADSTRING);
-		CartMenuCallback =Temp;
-		if (CartMenuCallback  != nullptr)
-			BuildDynaMenu();
-		return ;
+		static char string_buffer[MAX_LOADSTRING];
+
+		LoadString(gModuleInstance, IDS_MODULE_NAME, string_buffer, MAX_LOADSTRING);
+
+		return string_buffer;
 	}
+
+	__declspec(dllexport) const char* PakCatalogName()
+	{
+		static char string_buffer[MAX_LOADSTRING];
+
+		LoadString(gModuleInstance, IDS_CATNUMBER, string_buffer, MAX_LOADSTRING);
+
+		return string_buffer;
+	}
+
+	__declspec(dllexport) void PakInitialize(
+		void* const host_key,
+		const char* const configuration_path,
+		const pak_initialization_parameters* const parameters)
+	{
+		gHostKeyPtr = host_key;
+		CartMenuCallback = parameters->add_menu_item;
+		AssertInt = parameters->assert_interrupt;
+		strcpy(IniFile, configuration_path);
+
+		LoadConfig();
+		BuildCartridgeMenu();
+	}
+
 }
 
 extern "C"
 {
-	__declspec(dllexport) void ModuleConfig(unsigned char MenuID)
+	__declspec(dllexport) void PakMenuItemClicked(unsigned char MenuID)
 	{
 		HWND h_own = GetActiveWindow();
 		switch (MenuID)
@@ -146,7 +171,7 @@ extern "C"
 				break;
 			case 16:
 				if (g_hConfDlg == nullptr)
-					g_hConfDlg = CreateDialog(g_hinstDLL,(LPCTSTR)IDD_CONFIG,h_own,(DLGPROC)Config);
+					g_hConfDlg = CreateDialog(gModuleInstance,(LPCTSTR)IDD_CONFIG,h_own,(DLGPROC)Config);
 				ShowWindow(g_hConfDlg,1);
 				break;
 			case 17:
@@ -157,34 +182,15 @@ extern "C"
 				SaveConfig();
 				break;
 		}
-		BuildDynaMenu();
+		BuildCartridgeMenu();
 		return;
 	}
 }
 
-extern "C"
-{
-	__declspec(dllexport) void SetIniPath (const char *IniFilePath)
-	{
-		strcpy(IniFile,IniFilePath);
-		LoadConfig();
-		return;
-	}
-}
-
-// This captures the Fuction transfer point for the CPU assert interupt
-extern "C"
-{
-	__declspec(dllexport) void AssertInterupt(AssertInteruptModuleCallback Dummy)
-	{
-		AssertInt=Dummy;
-		return;
-	}
-}
 
 extern "C"
 {
-	__declspec(dllexport) void PackPortWrite(unsigned char Port,unsigned char Data)
+	__declspec(dllexport) void PakWritePort(unsigned char Port,unsigned char Data)
 	{
 #ifdef COMBINE_BECKER
 		if (BeckerEnabled && (Port == 0x42)) {
@@ -202,7 +208,7 @@ extern "C"
 
 extern "C"
 {
-	__declspec(dllexport) unsigned char PackPortRead(unsigned char Port)
+	__declspec(dllexport) unsigned char PakReadPort(unsigned char Port)
 	{
 #ifdef COMBINE_BECKER
 		if (BeckerEnabled && ((Port == 0x41) | (Port== 0x42 )))
@@ -216,7 +222,7 @@ extern "C"
 
 extern "C"
 {
-	__declspec(dllexport) void HeartBeat()
+	__declspec(dllexport) void PakProcessHorizontalSync()
 	{
 		PingFdc();
 		return;
@@ -225,7 +231,7 @@ extern "C"
 
 extern "C"
 {
-	__declspec(dllexport) unsigned char PakMemRead8(unsigned short Address)
+	__declspec(dllexport) unsigned char PakReadMemoryByte(unsigned short Address)
 	{
 		return(RomPointer[SelectRom][Address & (EXTROMSIZE-1)]);
 	}
@@ -233,17 +239,17 @@ extern "C"
 
 extern "C"
 {
-	__declspec(dllexport) void ModuleStatus(char *MyStatus)
+	__declspec(dllexport) void PakGetStatus(char* text_buffer, size_t buffer_size)
 	{
 		char diskstat[64];
-		DiskStatus(diskstat);
-		strcpy(MyStatus,diskstat);
+		DiskStatus(diskstat, sizeof(diskstat));
+		strcpy(text_buffer,diskstat);
 #ifdef COMBINE_BECKER
 		char beckerstat[64];
 		if(BeckerEnabled) {
 			becker_status(beckerstat);
-			strcat(MyStatus," | ");
-			strcat(MyStatus,beckerstat);
+			strcat(text_buffer," | ");
+			strcat(text_buffer,beckerstat);
 		}
 #endif
 		return ;
@@ -408,7 +414,7 @@ void Load_Disk(unsigned char disk)
 		if (hr == INVALID_HANDLE_VALUE) {
 			NewDiskNumber = disk;
 			//DialogBox will set CreateFlag = 0 on cancel
-			DialogBox(g_hinstDLL, (LPCTSTR)IDD_NEWDISK, h_own, (DLGPROC)NewDisk);
+			DialogBox(gModuleInstance, (LPCTSTR)IDD_NEWDISK, h_own, (DLGPROC)NewDisk);
 		} else {
 			CloseHandle(hr);
 		}
@@ -432,57 +438,57 @@ unsigned char SetChip(unsigned char Tmp)
 	return SelectRom;
 }
 
-void BuildDynaMenu()
+void BuildCartridgeMenu()
 {
 	char TempMsg[64]="";
 	char TempBuf[MAX_PATH]="";
 	if (CartMenuCallback ==nullptr)
 		MessageBox(g_hConfDlg,"No good","Ok",0);
 
-	CartMenuCallback( "", MID_BEGIN, MIT_Head);
-	CartMenuCallback( "", MID_ENTRY, MIT_Seperator);
+	CartMenuCallback(gHostKey, "", MID_BEGIN, MIT_Head);
+	CartMenuCallback(gHostKey, "", MID_ENTRY, MIT_Seperator);
 
-	CartMenuCallback( "FD-502 Drive 0",MID_ENTRY,MIT_Head);
-	CartMenuCallback( "Insert",ControlId(10),MIT_Slave);
+	CartMenuCallback(gHostKey, "FD-502 Drive 0",MID_ENTRY,MIT_Head);
+	CartMenuCallback(gHostKey, "Insert",ControlId(10),MIT_Slave);
 	strcpy(TempMsg,"Eject: ");
 	strcpy(TempBuf,Drive[0].ImageName);
 	PathStripPath(TempBuf);
 	strcat(TempMsg,TempBuf);
-	CartMenuCallback( TempMsg,ControlId(11),MIT_Slave);
+	CartMenuCallback(gHostKey, TempMsg,ControlId(11),MIT_Slave);
 
-	CartMenuCallback( "FD-502 Drive 1",MID_ENTRY,MIT_Head);
-	CartMenuCallback( "Insert",ControlId(12),MIT_Slave);
+	CartMenuCallback(gHostKey, "FD-502 Drive 1",MID_ENTRY,MIT_Head);
+	CartMenuCallback(gHostKey, "Insert",ControlId(12),MIT_Slave);
 	strcpy(TempMsg,"Eject: ");
 	strcpy(TempBuf,Drive[1].ImageName);
 	PathStripPath(TempBuf);
 	strcat(TempMsg,TempBuf);
-	CartMenuCallback( TempMsg,ControlId(13),MIT_Slave);
+	CartMenuCallback(gHostKey, TempMsg,ControlId(13),MIT_Slave);
 
-	CartMenuCallback( "FD-502 Drive 2",MID_ENTRY,MIT_Head);
-	CartMenuCallback( "Insert",ControlId(14),MIT_Slave);
+	CartMenuCallback(gHostKey, "FD-502 Drive 2",MID_ENTRY,MIT_Head);
+	CartMenuCallback(gHostKey, "Insert",ControlId(14),MIT_Slave);
 	strcpy(TempMsg,"Eject: ");
 	strcpy(TempBuf,Drive[2].ImageName);
 	PathStripPath(TempBuf);
 	strcat(TempMsg,TempBuf);
-	CartMenuCallback( TempMsg,ControlId(15),MIT_Slave);
+	CartMenuCallback(gHostKey, TempMsg,ControlId(15),MIT_Slave);
 
-	CartMenuCallback( "FD-502 Drive 3",MID_ENTRY,MIT_Head);
-	CartMenuCallback( "Insert",ControlId(17),MIT_Slave);
+	CartMenuCallback(gHostKey, "FD-502 Drive 3",MID_ENTRY,MIT_Head);
+	CartMenuCallback(gHostKey, "Insert",ControlId(17),MIT_Slave);
 	strcpy(TempMsg,"Eject: ");
 	strcpy(TempBuf,Drive[3].ImageName);
 	PathStripPath(TempBuf);
 	strcat(TempMsg,TempBuf);
-	CartMenuCallback( TempMsg,ControlId(18),MIT_Slave);
+	CartMenuCallback(gHostKey, TempMsg,ControlId(18),MIT_Slave);
 
-	CartMenuCallback( "FD-502 Config",ControlId(16),MIT_StandAlone);
-	CartMenuCallback("", MID_FINISH, MIT_Head);
+	CartMenuCallback(gHostKey, "FD-502 Config",ControlId(16),MIT_StandAlone);
+	CartMenuCallback(gHostKey,"", MID_FINISH, MIT_Head);
 }
 
 long CreateDisk (unsigned char Disk)
 {
 	NewDiskNumber=Disk;
 	HWND h_own = GetActiveWindow();
-	DialogBox(g_hinstDLL, (LPCTSTR)IDD_NEWDISK, h_own, (DLGPROC)NewDisk);
+	DialogBox(gModuleInstance, (LPCTSTR)IDD_NEWDISK, h_own, (DLGPROC)NewDisk);
 	return 0;
 }
 
@@ -665,7 +671,7 @@ void LoadConfig()  // Called on SetIniPath
 	becker_enable(BeckerEnabled);
 #endif
 
-	LoadString(g_hinstDLL,IDS_MODULE_NAME,ModName, MAX_LOADSTRING);
+	LoadString(gModuleInstance,IDS_MODULE_NAME,ModName, MAX_LOADSTRING);
 	GetPrivateProfileString("DefaultPaths", "FloppyPath", "", FloppyPath, MAX_PATH, IniFile);
 
 	SelectRom = GetPrivateProfileInt(ModName,"DiskRom",1,IniFile);  //0 External 1=TRSDOS 2=RGB Dos
@@ -701,7 +707,7 @@ void LoadConfig()  // Called on SetIniPath
 	ClockEnabled=GetPrivateProfileInt(ModName,"ClkEnable",1,IniFile);
 	SetTurboDisk(GetPrivateProfileInt(ModName, "TurboDisk", 1, IniFile));
 
-	BuildDynaMenu();
+	BuildCartridgeMenu();
 	return;
 }
 
@@ -710,7 +716,7 @@ void SaveConfig()
 	unsigned char Index=0;
 	char ModName[MAX_LOADSTRING]="";
 	char Temp[16]="";
-	LoadString(g_hinstDLL,IDS_MODULE_NAME,ModName, MAX_LOADSTRING);
+	LoadString(gModuleInstance,IDS_MODULE_NAME,ModName, MAX_LOADSTRING);
 	ValidatePath(RomFileName);
 	WritePrivateProfileInt(ModName,"DiskRom",SelectRom ,IniFile);
 	WritePrivateProfileString(ModName,"RomPath",RomFileName,IniFile);

FD502/fd502.h
@@ -19,8 +19,9 @@ This file is part of VCC (Virtual Color Computer).
 */
 #include <vcc/core/legacy_cartridge_definitions.h>
 
-extern AssertInteruptModuleCallback AssertInt;
-void BuildDynaMenu();
+extern void*const& gHostKey;
+extern PakAssertInteruptHostCallback AssertInt;
+void BuildCartridgeMenu();
 
 // FIXME: These need to be turned into a scoped enum and the signature of functions
 // that use them updated.

FD502/wd1793.cpp
@@ -245,7 +245,7 @@ int mount_disk_image(const char *filename,unsigned char drive)
 {
 	unsigned int Temp=0;
 	Temp=MountDisk(filename,drive);
-	BuildDynaMenu();
+	BuildCartridgeMenu();
 	return(!Temp);
 }
 
@@ -264,13 +264,12 @@ void unmount_disk_image(unsigned char drive)
 	return;
 }
 
-void DiskStatus(char *Status)
+void DiskStatus(char* text_buffer, size_t buffer_size)
 {
 	if (MotorOn==1) 
-		sprintf(Status,"FD-502:Drv:%1.1i %s Trk:%2.2i Sec:%2.2i Hd:%1.1i",CurrentDisk,ImageFormat[Drive[CurrentDisk].ImageType],Drive[CurrentDisk].HeadPosition,SectorReg,Side);
+		sprintf(text_buffer,"FD-502:Drv:%1.1i %s Trk:%2.2i Sec:%2.2i Hd:%1.1i",CurrentDisk,ImageFormat[Drive[CurrentDisk].ImageType],Drive[CurrentDisk].HeadPosition,SectorReg,Side);
 	else
-		sprintf(Status,"FD-502:Idle");
-	return;
+		sprintf(text_buffer,"FD-502:Idle");
 }
 
 unsigned char MountDisk(const char *FileName,unsigned char disk)
@@ -953,7 +952,7 @@ void DispatchCommand(unsigned char Tmp)
 			StatusReg=READY;
 			ExecTimeWaiter=1;
 			if ((Tmp & 15) != 0)
-				AssertInt(INT_NMI,IS_NMI);
+				AssertInt(gHostKey, INT_NMI,IS_NMI);
 //			WriteLog("FORCEINTERUPT",0);
 			break;
 
@@ -1298,7 +1297,7 @@ long GetSectorInfo (SectorInfo *Sector,const unsigned char *TempBuffer)
 void CommandDone()
 {
 	if (InteruptEnable)
-		AssertInt(INT_NMI,IS_NMI);
+		AssertInt(gHostKey, INT_NMI,IS_NMI);
 	TransferBufferSize=0;
 	CurrentCommand=IDLE;
 }

FD502/wd1793.h
@@ -22,7 +22,7 @@ unsigned char disk_io_read(unsigned char port);
 void disk_io_write(unsigned char data,unsigned char port);	
 int mount_disk_image(const char *,unsigned char );
 void unmount_disk_image(unsigned char drive);
-void DiskStatus(char *);
+void DiskStatus(char* text_buffer, size_t buffer_size);
 void PingFdc();
 unsigned char SetTurboDisk( unsigned char);
 //unsigned char UseKeyboardLeds(unsigned char);

GMC/Cartridge.cpp
@@ -3,8 +3,8 @@
 
 namespace detail
 {
-	void NullAssetCartridgeLine(bool) {}
-	void NullAddMenuItem(const char *, int, MenuItemType) {}
+	void NullAssetCartridgeLine(void*, bool) {}
+	void NullAddMenuItem(void*, const char *, int, MenuItemType) {}
 }
 
 
@@ -60,7 +60,13 @@ std::string Cartridge::GetCatalogId() const
 
 
 
-void Cartridge::SetCartLineAssertCallback(AssertCartridgeLineModuleCallback callback)
+void Cartridge::SetHostKey(void* key)
+{
+	m_HostKey = key;
+}
+
+
+void Cartridge::SetCartLineAssertCallback(PakAssertCartridgeLineHostCallback callback)
 {
 	AssetCartridgeLinePtr = callback;
 }
@@ -74,7 +80,7 @@ void Cartridge::SetConfigurationPath(std::string path)
 }
 
 
-void Cartridge::SetMenuBuilderCallback(AppendCartridgeMenuModuleCallback callback)
+void Cartridge::SetMenuBuilderCallback(PakAppendCartridgeMenuHostCallback callback)
 {
 	AddMenuItemPtr = callback;
 }

GMC/Cartridge.h
@@ -17,8 +17,9 @@ public:
 	virtual std::string GetName() const;
 	virtual std::string GetCatalogId() const;
 
-	virtual void SetCartLineAssertCallback(AssertCartridgeLineModuleCallback callback);
-	virtual void SetMenuBuilderCallback(AppendCartridgeMenuModuleCallback addMenuCallback);
+	virtual void SetHostKey(void* key);
+	virtual void SetCartLineAssertCallback(PakAssertCartridgeLineHostCallback callback);
+	virtual void SetMenuBuilderCallback(PakAppendCartridgeMenuHostCallback addMenuCallback);
 	virtual void SetConfigurationPath(std::string path);
 
 	virtual std::string GetStatusMessage() const;
@@ -35,12 +36,12 @@ protected:
 
 	void AssetCartridgeLine(bool state) const
 	{
-		AssetCartridgeLinePtr(state);
+		AssetCartridgeLinePtr(m_HostKey, state);
 	}
 
 	void AddMenuItem(const char * name, int id, MenuItemType type)
 	{
-		AddMenuItemPtr(name, id, type);
+		AddMenuItemPtr(m_HostKey, name, id, type);
 	}
 
 	virtual void LoadConfiguration(const std::string& filename);
@@ -48,25 +49,29 @@ protected:
 
 private:
 
-	friend void ModuleName(char *ModName, char *CatNumber, AppendCartridgeMenuModuleCallback addMenuCallback);
-	friend void ModuleStatus(char *statusBuffer);
-	friend void ModuleConfig(unsigned char menuId);
-	friend void SetIniPath(const char *iniFilePath);
-	friend void ModuleReset();
-	friend void SetCart(AssertCartridgeLineModuleCallback Pointer);
-	friend unsigned char PakMemRead8(unsigned short address);
-	friend void PackPortWrite(unsigned char port, unsigned char data);
-	friend unsigned char PackPortRead(unsigned char port);
-	friend unsigned short ModuleAudioSample();
+	friend const char* PakGetName();
+	friend const char* PakCatalogName();
+	friend void PakInitialize(
+		void* const host_key,
+		const char* const configuration_path,
+		const pak_initialization_parameters* const parameters);
+	friend void PakGetStatus(char* text_buffer, size_t buffer_size);
+	friend void PakMenuItemClicked(unsigned char menuId);
+	friend void PakReset();
+	friend unsigned char PakReadMemoryByte(unsigned short address);
+	friend void PakWritePort(unsigned char port, unsigned char data);
+	friend unsigned char PakReadPort(unsigned char port);
+	friend unsigned short PakSampleAudio();
 
 private:
 
 	static Cartridge*	m_Singleton;
 
+	void*				m_HostKey = nullptr;
 	std::string			m_Name;
 	std::string			m_CatalogId;
 	std::string			m_ConfigurationPath;
-	AssertCartridgeLineModuleCallback	AssetCartridgeLinePtr = detail::NullAssetCartridgeLine;
-	AppendCartridgeMenuModuleCallback 	AddMenuItemPtr = detail::NullAddMenuItem;
+	PakAssertCartridgeLineHostCallback	AssetCartridgeLinePtr = detail::NullAssetCartridgeLine;
+	PakAppendCartridgeMenuHostCallback 	AddMenuItemPtr = detail::NullAddMenuItem;
 };
 

GMC/CartridgeTrampolines.cpp
@@ -2,41 +2,41 @@
 #include "Cartridge.h"
 
 
-GMC_EXPORT void ModuleName(char *moduleName, char *catalogId, AppendCartridgeMenuModuleCallback addMenuCallback)
+GMC_EXPORT const char* PakGetName()
 {
-	Cartridge::m_Singleton->SetMenuBuilderCallback(addMenuCallback);
+	return Cartridge::m_Singleton->GetName().c_str();
+}
 
-	strcpy(moduleName, Cartridge::m_Singleton->GetName().c_str());
-	strcpy(catalogId, Cartridge::m_Singleton->GetCatalogId().c_str());
+GMC_EXPORT const char* PakCatalogName()
+{
+	return Cartridge::m_Singleton->GetCatalogId().c_str();
+}
+
+GMC_EXPORT void PakInitialize(
+	void* const host_key,
+	const char* const configuration_path,
+	const pak_initialization_parameters* const parameters)
+{
+	Cartridge::m_Singleton->SetHostKey(host_key);
+	Cartridge::m_Singleton->SetMenuBuilderCallback(parameters->add_menu_item);
+	Cartridge::m_Singleton->SetCartLineAssertCallback(parameters->assert_cartridge_line);
+	Cartridge::m_Singleton->SetConfigurationPath(configuration_path);
 }
 
 
-GMC_EXPORT void ModuleConfig(unsigned char menuId)
+GMC_EXPORT void PakMenuItemClicked(unsigned char menuId)
 {
 	Cartridge::m_Singleton->OnMenuItemSelected(menuId);
 }
 
 
-GMC_EXPORT void SetIniPath(const char *iniFilePath)
-{
-	Cartridge::m_Singleton->SetConfigurationPath(iniFilePath);
-}
-
-
-GMC_EXPORT void ModuleReset()
+GMC_EXPORT void PakReset()
 {
 	Cartridge::m_Singleton->OnReset();
 }
 
 
-GMC_EXPORT void SetCart(AssertCartridgeLineModuleCallback callback)
-{
-	Cartridge::m_Singleton->SetCartLineAssertCallback(callback);
-}
-
-
-
-GMC_EXPORT void ModuleStatus(char *statusBuffer)
+GMC_EXPORT void PakGetStatus(char* text_buffer, size_t buffer_size)
 {
 	auto message(Cartridge::m_Singleton->GetStatusMessage());
 	if (message.size() > 63)
@@ -44,11 +44,11 @@ GMC_EXPORT void ModuleStatus(char *statusBuffer)
 		message.resize(63);
 	}
 
-	strcpy(statusBuffer, message.c_str());
+	strcpy(text_buffer, message.c_str());
 }
 
 
-GMC_EXPORT unsigned char PakMemRead8(unsigned short address)
+GMC_EXPORT unsigned char PakReadMemoryByte(unsigned short address)
 {
 	return Cartridge::m_Singleton->OnReadMemory(address);
 }
@@ -56,20 +56,20 @@ GMC_EXPORT unsigned char PakMemRead8(unsigned short address)
 
 
 
-GMC_EXPORT void PackPortWrite(unsigned char port, unsigned char data)
+GMC_EXPORT void PakWritePort(unsigned char port, unsigned char data)
 {
 	Cartridge::m_Singleton->OnWritePort(port, data);
 }
 
 
-GMC_EXPORT unsigned char PackPortRead(unsigned char port)
+GMC_EXPORT unsigned char PakReadPort(unsigned char port)
 {
 	return Cartridge::m_Singleton->OnReadPort(port);
 }
 
 
 // This gets called at the end of every scan line 262 Lines * 60 Frames = 15780 Hz 15720
-GMC_EXPORT unsigned short ModuleAudioSample()
+GMC_EXPORT unsigned short PakSampleAudio()
 {
 	return Cartridge::m_Singleton->UpdateAudio();
 }

commit d7ceb1291e5b5bd5169e87eeee21656377a826dd

ACIA call BuildCartridgeMenu() on init
Fixes Acia broken by eda4552 Refactor C API cartridge interface #466

acia/acia.cpp
@@ -122,19 +122,18 @@ extern "C"
 		CartMenuCallback = context->add_menu_item;
 		AssertInt = context->assert_interrupt;
 		strcpy(IniFile, configuration_path);
-
 		LoadConfig();
+		BuildCartridgeMenu();
 		LoadExtRom("RS232.ROM");
 		sc6551_init();
 	}
+
 	__declspec(dllexport) void PakTerminate()
 	{
 		CloseCartDialog(g_hDlg);
 		sc6551_close();
 		AciaStat[0]='\0';
 	}
-	
-
 }
 
 //-----------------------------------------------------------------------