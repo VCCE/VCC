f3486fc28dadf1b00e0313b326d826096ae0aa18
ejaquay <ejjaquay@gmail.com>
2025-10-30 10:41:51 -0400

    Change workflow for Build Logging only
    Buildlogging builds VCC with Logging enabled.  This should
    be sufficient for workflow testing and saves workflow time.

--- BuildLogging.bat
+++ BuildLogging.bat
@@ -4,9 +4,6 @@
 set VCC_CONFIG=Release
 if exist "%VSINSTALLDIR%\MSBuild\Current\Bin\MSBuild.exe" (
   msbuild vcc.sln /t:Clean /p:Configuration=%VCC_CONFIG% /p:Platform=x86
-  set _CL_=
-  call Build.bat
-  msbuild vcc.sln /t:Clean /p:Configuration=%VCC_CONFIG% /p:Platform=x86
   set _CL_=/DUSE_LOGGING
   call Build.bat
 ) else (
