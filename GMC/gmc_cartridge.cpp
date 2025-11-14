d64b985e221a3976779a7461ea7552c299f9baf1
Chet Simpson <3150309+ChetSimpson@users.noreply.github.com>
2025-11-14 07:29:59 -0700

    convert gmc cartridge to use c api

--- GMC/gmc_cartridge.cpp
+++ GMC/gmc_cartridge.cpp
@@ -1,19 +1,21 @@
-#include "gmc_cartridge.h"
+#include "sn76496.h"
 #include "resource.h"
+#include <vcc/bus/cartridge_capi.h>
+#include <vcc/devices/rom/banked_rom_image.h>
 #include <vcc/common/DialogOps.h>
 #include <vcc/utils/persistent_value_store.h>
 #include <vcc/utils/winapi.h>
 #include <vcc/utils/filesystem.h>
 #include "../CartridgeMenu.h"
 #include <Windows.h>
-#include <vcc/bus/cartridge_capi.h>
+
 
 namespace
 {
 
 	HINSTANCE gModuleInstance;
 
-	using context_type = ::vcc::bus::cartridge_context;
+	using context_type = cartridge_capi_context;
 	using path_type = ::std::string;
 	using rom_image_type = ::vcc::devices::rom::banked_rom_image;
 
@@ -28,12 +30,14 @@ namespace
 		static const unsigned char psg_io = 0x41;
 	};
 
-	const std::unique_ptr<context_type> context_;
+	void* gHostKey = nullptr;
+	context_type* context_ = nullptr;
 	rom_image_type rom_image_;
 	SN76489Device psg_;
 
 	const path_type configuration_section_id_("GMC-SN74689");
 	const path_type configuration_rom_key_id_("ROM");
+	std::string configuration_path_;
 
 
 	std::string SelectROMFile()
@@ -53,6 +57,8 @@ namespace
 
 }
 
+void load_rom(const path_type& filename, bool reset_on_load);
+void build_menu();
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
 {
@@ -65,142 +71,151 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
 }
 
 
-__declspec(dllexport) const char* PakGetName()
+extern "C"
 {
-	static const auto name(::vcc::utils::load_string(gModuleInstance, IDS_MODULE_NAME));
-	return name.c_str();
-}
 
-__declspec(dllexport) const char* PakGetCatalogId()
-{
-	static const auto catalog_id(::vcc::utils::load_string(gModuleInstance, IDS_CATNUMBER));
-	return catalog_id.c_str();
-}
-
-__declspec(dllexport) const char* PakGetDescription()
-{
-	static const auto description(::vcc::utils::load_string(gModuleInstance, IDS_DESCRIPTION));
-	return description.c_str();
-}
-
-
-__declspec(dllexport) void PakInitialize(
-	void* const host_key,
-	const char* const configuration_path,
-	const cartridge_capi_context* const context)
-{
-	::vcc::utils::persistent_value_store settings(context_->configuration_path());
-	const auto selected_file(settings.read(configuration_section_id_, configuration_rom_key_id_));
-
-	load_rom(selected_file, false);
-	build_menu();
-}
-
-
-__declspec(dllexport) void PakReset()
-{
-	psg_.device_start();
-	build_menu();
-}
-
-
-__declspec(dllexport) void PakWritePort(unsigned char port,unsigned char data)
-{
-	switch (port)
+	__declspec(dllexport) const char* PakGetName()
 	{
-	case mmio_registers::select_bank:
-		rom_image_.select_bank(data);
-		break;
-
-	case mmio_registers::psg_io:
-		psg_.write(data);
-		break;
-	}
-}
-
-__declspec(dllexport) unsigned char PakReadPort(unsigned char port)
-{
-	if (port == mmio_registers::select_bank)
-	{
-		return rom_image_.selected_bank();
+		static const auto name(::vcc::utils::load_string(gModuleInstance, IDS_MODULE_NAME));
+		return name.c_str();
 	}
 
-	return 0;
-}
-
-__declspec(dllexport) unsigned char PakReadMemoryByte(unsigned short address)
-{
-	return rom_image_.read_memory_byte(address);
-}
-
-__declspec(dllexport) void PakGetStatus(char* text_buffer, size_t buffer_size)
-{
-	std::string message("GMC Active");
-
-	const auto activeRom(::vcc::utils::get_filename(rom_image_.filename()));
-	if (!rom_image_.empty())
+	__declspec(dllexport) const char* PakGetCatalogId()
 	{
-		message += " (" + activeRom + " Loaded)";
-	}
-	else
-	{
-		message += activeRom.empty() ? " (No ROM Selected)" : "(Unable to load `" + activeRom + "`)";
+		static const auto catalog_id(::vcc::utils::load_string(gModuleInstance, IDS_CATNUMBER));
+		return catalog_id.c_str();
 	}
 
-	if(message.size() >= buffer_size)
+	__declspec(dllexport) const char* PakGetDescription()
 	{
-		message.reserve(buffer_size - 1);
+		static const auto description(::vcc::utils::load_string(gModuleInstance, IDS_DESCRIPTION));
+		return description.c_str();
 	}
 
-	strcpy(text_buffer, message.c_str());
-}
 
-__declspec(dllexport) unsigned short PakSampleAudio()
-{
-	SN76489Device::stream_sample_t lbuffer = 0;
-	SN76489Device::stream_sample_t rbuffer = 0;
-
-	return psg_.sound_stream_update(lbuffer, rbuffer);
-}
-
-void gmc_cartridge::menu_item_clicked(unsigned char menuId)
-{
-	if (menuId == menu_item_ids::select_rom)
+	__declspec(dllexport) void PakInitialize(
+		void* const host_key,
+		const char* const configuration_path,
+		const cartridge_capi_context* const context)
 	{
-		const auto selected_file(SelectROMFile());
-		if (selected_file.empty())
-		{
-			return;
-		}
+		gHostKey = host_key;
+		configuration_path_ = configuration_path;
 
-		::vcc::utils::persistent_value_store settings(context_->configuration_path());
-		settings.write(
-			configuration_section_id_,
-			configuration_rom_key_id_,
-			selected_file);
+		::vcc::utils::persistent_value_store settings(configuration_path);
+		const auto selected_file(settings.read(configuration_section_id_, configuration_rom_key_id_));
 
-		load_rom(selected_file, true);
+		load_rom(selected_file, false);
 		build_menu();
 	}
+
+
+	__declspec(dllexport) void PakReset()
+	{
+		psg_.device_start();
+		build_menu();
+	}
+
+
+	__declspec(dllexport) void PakWritePort(unsigned char port, unsigned char data)
+	{
+		switch (port)
+		{
+		case mmio_registers::select_bank:
+			rom_image_.select_bank(data);
+			break;
+
+		case mmio_registers::psg_io:
+			psg_.write(data);
+			break;
+		}
+	}
+
+	__declspec(dllexport) unsigned char PakReadPort(unsigned char port)
+	{
+		if (port == mmio_registers::select_bank)
+		{
+			return rom_image_.selected_bank();
+		}
+
+		return 0;
+	}
+
+	__declspec(dllexport) unsigned char PakReadMemoryByte(unsigned short address)
+	{
+		return rom_image_.read_memory_byte(address);
+	}
+
+	__declspec(dllexport) void PakGetStatus(char* text_buffer, size_t buffer_size)
+	{
+		std::string message("GMC Active");
+
+		const auto activeRom(::vcc::utils::get_filename(rom_image_.filename()));
+		if (!rom_image_.empty())
+		{
+			message += " (" + activeRom + " Loaded)";
+		}
+		else
+		{
+			message += activeRom.empty() ? " (No ROM Selected)" : "(Unable to load `" + activeRom + "`)";
+		}
+
+		if (message.size() >= buffer_size)
+		{
+			message.reserve(buffer_size - 1);
+		}
+
+		strcpy(text_buffer, message.c_str());
+	}
+
+	__declspec(dllexport) unsigned short PakSampleAudio()
+	{
+		SN76489Device::stream_sample_t lbuffer = 0;
+		SN76489Device::stream_sample_t rbuffer = 0;
+
+		return psg_.sound_stream_update(lbuffer, rbuffer);
+	}
+
+	__declspec(dllexport) void PakMenuItemClicked(unsigned char menuId)
+	{
+		if (menuId == menu_item_ids::select_rom)
+		{
+			const auto selected_file(SelectROMFile());
+			if (selected_file.empty())
+			{
+				return;
+			}
+
+			::vcc::utils::persistent_value_store settings(configuration_path_);
+			settings.write(
+				configuration_section_id_,
+				configuration_rom_key_id_,
+				selected_file);
+
+			load_rom(selected_file, true);
+			build_menu();
+		}
+	}
+
 }
 
 
-void gmc_cartridge::build_menu()
+void build_menu()
 {
-	context_->add_menu_item("", MID_BEGIN, MIT_Head);
-	context_->add_menu_item("", MID_ENTRY, MIT_Seperator);
-	context_->add_menu_item("Select GMC ROM", ControlId(menu_item_ids::select_rom), MIT_StandAlone);
-	context_->add_menu_item("", MID_FINISH, MIT_Head);
+	context_->add_menu_item(gHostKey, "", MID_BEGIN, MIT_Head);
+	context_->add_menu_item(gHostKey, "", MID_ENTRY, MIT_Seperator);
+	context_->add_menu_item(gHostKey, "Select GMC ROM", ControlId(menu_item_ids::select_rom), MIT_StandAlone);
+	context_->add_menu_item(gHostKey, "", MID_FINISH, MIT_Head);
 }
 
-void gmc_cartridge::load_rom(const path_type& filename, bool reset_on_load)
+void load_rom(const path_type& filename, bool reset_on_load)
 {
 	if (!filename.empty() && rom_image_.load(filename))
 	{
-		context_->assert_cartridge_line(true);
+		context_->assert_cartridge_line(gHostKey, true);
 		if (reset_on_load)
 		{
-			context_->reset();
+			// FIXME: This functionality is not available in the C API
+			//context_->reset(gHostKey);
 		}
 	}
 	else
--- GMC/main.cpp
+++
@@ -1,5 +0,0 @@
-#include "gmc_cartridge.h"
-#include <vcc/bus/cartridge_factory.h>
-#include <Windows.h>
-
-
