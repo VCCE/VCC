682596c1d1897850870282d5e440966701dba27b
ejaquay <ejjaquay@gmail.com>
2025-12-26 16:42:40 -0500

    Clean up orch90 code

--- orch90/orch90.cpp
+++ orch90/orch90.cpp
@@ -15,43 +15,34 @@ This file is part of VCC (Virtual Color Computer).
     You should have received a copy of the GNU General Public License
     along with VCC (Virtual Color Computer).  If not, see <http://www.gnu.org/licenses/>.
 */
+
+//==========================================================================
+// Orch90 is perhaps the simplest pak, it uses PakSampleAudio() to simulate
+// two 8-bit digital-to-analog converters.
+//==========================================================================
+
 #include <Windows.h>
 #include <stdio.h>
-#include "resource.h" 
+#include "resource.h"
 #include <vcc/core/FileOps.h>
 #include <vcc/bus/cpak_cartridge_definitions.h>
 #include <vcc/core/limits.h>
+#include <vcc/core/logger.h>
 
 static HINSTANCE gModuleInstance;
 static void* gCallbackContext = nullptr;
 static PakAssertCartridgeLineHostCallback PakSetCart = nullptr;
 static unsigned char LeftChannel=0,RightChannel=0;
-unsigned char LoadExtRom(const char *);
 static unsigned char Rom[8192];
+bool LoadRom();
 
-
-BOOL WINAPI DllMain(
-    HINSTANCE hinstDLL,  // handle to DLL module
-    DWORD fdwReason,     // reason for calling function
-    LPVOID lpReserved )  // reserved
+// DLL exports
+extern "C"
 {
-	if (fdwReason == DLL_PROCESS_ATTACH) 
-	{
-		gModuleInstance = hinstDLL;
-	}
-
-	return TRUE;
-}
-
-
-extern "C" 
-{          
-
 	__declspec(dllexport) const char* PakGetName()
 	{
 		static char string_buffer[MAX_LOADSTRING];
 
-		//LoadString(gModuleInstance, IDS_MODULE_NAME, string_buffer, MAX_LOADSTRING);
 		strcpy(string_buffer, "Orchestra-90");
 
 		return string_buffer;
@@ -81,21 +72,17 @@ extern "C"
 		HWND hVccWnd,
 		const cpak_callbacks* const callbacks)
 	{
+		DLOG_C("orch90 init\n");
 		gCallbackContext = callback_context;
 		PakSetCart = callbacks->assert_cartridge_line;
 	}
 
-}
-
-
-extern "C" 
-{         
 	__declspec(dllexport) void PakWritePort(unsigned char Port,unsigned char Data)
 	{
 		switch (Port)
 		{
 		case 0x7A:
-			RightChannel=Data;			
+			RightChannel=Data;
 			break;
 
 		case 0x7B:
@@ -104,72 +91,58 @@ extern "C"
 		}
 		return;
 	}
-}
 
-
-extern "C"
-{
 	__declspec(dllexport) unsigned char PakReadPort(unsigned char /*Port*/)
 	{
 		return 0;
 	}
-}
 
-extern "C"
-{
 	__declspec(dllexport) void PakReset()
 	{
-		char RomPath[MAX_PATH];
-
-		memset(Rom, 0xff, 8192);
-		GetModuleFileName(nullptr, RomPath, MAX_PATH);
-		PathRemoveFileSpec(RomPath);
-		strcpy(RomPath, "ORCH90.ROM");
-		
-		if (LoadExtRom(RomPath))	//If we can load the rom them assert cart 
-			PakSetCart(gCallbackContext, 1);
+		DLOG_C("orch90 reset\n");
+		if (LoadRom()) PakSetCart(gCallbackContext, 1);
 	}
-}
 
-
-extern "C"
-{
 	__declspec(dllexport) unsigned char PakReadMemoryByte(unsigned short Address)
 	{
 		return(Rom[Address & 8191]);
 	}
-}
 
-
-
-// This gets called at the end of every scan line 262 Lines * 60 Frames = 15780 Hz 15720
-extern "C" 
-{          
 	__declspec(dllexport) unsigned short PakSampleAudio()
 	{
-		
+		//This is called every scan line 262 Lines * 60 Frames = 15780 Hz 15720
 		return((LeftChannel<<8) | RightChannel) ;
 	}
 }
 
-
-
-unsigned char LoadExtRom(const char *FilePath)	//Returns 1 on if loaded
+// DLLMain
+BOOL WINAPI DllMain(
+    HINSTANCE hinstDLL,  // handle to DLL module
+    DWORD fdwReason,     // reason for calling function
+    LPVOID lpReserved )  // reserved
 {
+	if (fdwReason == DLL_PROCESS_ATTACH)
+		gModuleInstance = hinstDLL;
+	return TRUE;
+}
 
-	FILE *rom_handle = nullptr;
+//Load orch90.rom
+bool LoadRom()
+{
+	memset(Rom, 0xff, 8192);
+
+	// Assume rom is in same directory as orch90.dll
+	char RomPath[MAX_PATH];
+	GetModuleFileName(nullptr, RomPath, MAX_PATH);
+	PathRemoveFileSpec(RomPath);
+	strcpy(RomPath, "ORCH90.ROM");
+
+	FILE *rom_handle = fopen(RomPath, "rb");
+	if (rom_handle == nullptr) return false;
+
 	unsigned short index = 0;
-	unsigned char RetVal = 0;
-
-	rom_handle = fopen(FilePath, "rb");
-	if (rom_handle == nullptr)
-		memset(Rom, 0xFF, 8192);
-	else
-	{
-		while ((feof(rom_handle) == 0) & (index<8192))
-			Rom[index++] = fgetc(rom_handle);
-		RetVal = 1;
-		fclose(rom_handle);
-	}
-	return RetVal;
+	while ((feof(rom_handle) == 0) & (index<8192))
+		Rom[index++] = fgetc(rom_handle);
+	fclose(rom_handle);
+	return true;
 }
