14823b67812fbc6a8c22cb6277003fe75dfbc7e0
ejaquay <ejjaquay@gmail.com>
2025-12-29 11:59:10 -0500

    Move solution output from __obj to __bin directory

--- Build.bat
+++ Build.bat
@@ -5,8 +5,7 @@ set VCC_CONFIG=Release
 if exist "%VSINSTALLDIR%\MSBuild\Current\Bin\MSBuild.exe" (
   msbuild vcc.sln /m /p:Configuration=%VCC_CONFIG% /p:Platform=x86
   if errorlevel 0 (
-    echo Find Vcc.exe in %cd%\__obj\Win32\%VCC_CONFIG%\vcc\out\vcc.exe
-    echo Find [name].dll in %cd%\__obj\Win32\%VCC_CONFIG%\[name]\out\[name].dll
+    echo Find VCC binaries in %cd%\__bin\Win32\%VCC_CONFIG%
   )
 ) else (
   echo Build must be run from Developer Command Prompt for VS 2022
